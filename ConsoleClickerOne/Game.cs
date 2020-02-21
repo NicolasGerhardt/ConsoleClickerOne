621f3ac07f9cae1f8eccf58411a1501bb243a31c
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-21 14:36:33 -0500

    Added core project files with core game loop

---
+++ ConsoleClickerOne/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleClickerOne
+{
+    public class Game
+    {
+        public int Clicks { get; set; }
+        public bool GameOver { get; set; }
+
+        public Game()
+        {
+            Clicks = 0;
+            GameOver = false;
+        }
+
+        public void GameLoop()
+        {
+            while (!GameOver)
+            {
+                Console.Clear();
+                Console.WriteLine($"Clicks: {Clicks}");
+                Console.Write($"Press enter to click");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    click(1);
+                }
+            }
+
+            Console.WriteLine("GameOver");
+        }
+
+        public void click(int n)
+        {
+            Clicks += n;
+        }
+    }
+}
